5fa5e02307c032fa75c363883a6efcf9226491f1
amirtaherkhani <amirtaherkhani@outlook.com>
2024-05-02 07:19:54 +0330

    Add nodemon

--- next-mail/src/app/page.tsx
+++ next-mail/src/app/page.tsx
@@ -1051,7 +1051,7 @@ export default function home() {
           </DropdownMenu>
         </div>
       </header>
-      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col  bg-gray-300 gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col bg-zinc-200 gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
         <div className="mx-auto grid w-full max-w-6xl gap-2">
           <h1 className="text-3xl font-semibold">Settings</h1>
         </div>
